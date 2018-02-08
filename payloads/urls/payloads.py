23944a30e63538c2b44515ed9a26bb2db5f1a195
Gerard Wagener <gerard.wagener@circl.lu>
2018-02-08 21:54:18 +0100

    Iterate over files

--- payloads/urls/payloads.py
+++ payloads/urls/payloads.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sqlite3
 import argparse
+import os
 
 class Payloads:
     def create_new_index(self, database):
@@ -16,13 +17,19 @@ class Payloads:
          """
         cur.execute(sql)
 
+    def update_index(self, database, repository):
+        for uuid in os.listdir(repository):
+            print (uuid)
+
 obj = Payloads()
 
 parser = argparse.ArgumentParser(description="Sighting tests for files")
 parser.add_argument("--create", action="store_true")
-parser.add_argument("--repo", type=str, nargs=1, required=False)
+parser.add_argument("--repository", type=str, nargs=1, required=False)
 parser.add_argument("--database", type=str, nargs=1, required=True)
 args = parser.parse_args()
 
 if args.create:
     obj.create_new_index(args.database[0])
+
+obj.update_index(args.database[0], args.repository[0])
