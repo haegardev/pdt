aee738da83274597765275f549884578654a0077
Gerard Wagener <gerard.wagener@circl.lu>
2018-06-25 11:40:29 +0200

    Moved out HASHDEBUG switch

--- pcaptest/pibs.c
+++ pcaptest/pibs.c
@@ -40,7 +40,6 @@
 #define SZBIN 4
 #define NBINSCALE 2 // Scaling factor of the entire datastructure
 
-#define HASHDEBUG 1
 #define HDBG(...) if (HASHDEBUG) fprintf(stderr, __VA_ARGS__)
 
 typedef struct pibs_header_s {
